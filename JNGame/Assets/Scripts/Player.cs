20afe715418e9bdca79f3b998cc66919f0bfdc03
redjcfm <redjcfm@users.noreply.github.com>
2018-05-05 13:49:14 -0300

    teste

---
+++ JNGame/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 4f;
+
+    Animator anim;
+
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+
+        Vector3 mov = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0
+        );
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            transform.position + mov,
+            speed * Time.deltaTime
+        );
+
+        anim.SetFloat("movX", mov.x);
+        anim.SetFloat("movY", mov.y);
+
+    }
+
+}
