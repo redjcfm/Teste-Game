7222355ffc4524b54249ecf3a4e363ca038560e3
redjcfm <redjcfm@users.noreply.github.com>
2018-05-06 14:46:18 -0300

    Codigo do Player atualizado
    Mudei o Vetor para 2D pq o z a gente nao usa.
    
    adicionei  para verificar se o personagem esta se movendo, se sim pega a animacao de caminhar, se nao fica a animacao de parado

--- JNGame/Assets/Scripts/Player.cs
+++ JNGame/Assets/Scripts/Player.cs
@@ -8,32 +8,40 @@ public class Player : MonoBehaviour
     public float speed = 4f;
 
     Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;  
 
     void Start()
     {
-
         anim = GetComponent<Animator>();
-
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
 
-        Vector3 mov = new Vector3(
+        // movimento 2D
+        mov = new Vector2(
             Input.GetAxisRaw("Horizontal"),
-            Input.GetAxisRaw("Vertical"),
-            0
+            Input.GetAxisRaw("Vertical")
         );
 
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            transform.position + mov,
-            speed * Time.deltaTime
-        );
-
-        anim.SetFloat("movX", mov.x);
-        anim.SetFloat("movY", mov.y);
+        // animacoes
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+            anim.SetBool("walking", true);
+        }
+        else
+        {
+            anim.SetBool("walking", false);
+        }
 
     }
 
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    }
 }
